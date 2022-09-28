9274a539065f3af032d632a47829596799cc0a9d
Rendi Wijiatmoko <rendiwijiatmoko97@gmail.com>
2022-09-28 22:56:16 +0700

    get all movies

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "526371567", Title: "Movie one", Director: &Director{Firstname: "Momo", Lastname: "Wijiatmoko"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "326371567", Title: "Movie two", Director: &Director{Firstname: "Momo", Lastname: "Wijiatmoko"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+
+	fmt.Printf("Starting server at port :8000")
+	log.Fatal(http.ListenAndServe("8000", r))
+}
